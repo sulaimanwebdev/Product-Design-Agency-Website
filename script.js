0aada320be2745f5819c7812b787be1787425e96
Muhammad Sulaiman <74793541+sulaimanwebdev@users.noreply.github.com>
2021-05-03 14:26:00 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let burger = document.querySelector('#burger');
-let nav = document.querySelector('.nav')
-burger.addEventListener('click', ()=>{
-    burger.classList.toggle('fa-times')
-    
-    nav.classList.toggle('show')
- 
-})
